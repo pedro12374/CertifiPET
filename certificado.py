6bd84661d7e2fe35823ac7695bf93d3867fc98d3
pedro12374 <pedrohaerter@hotmail.com>
2017-09-20 14:00:37 -0300

    att

--- certificado.py
+++ certificado.py
@@ -2,21 +2,15 @@
 import os,datetime,locale
 locale.setlocale(locale.LC_TIME, "pt_BR.UTF-8")
 
-
-
-
 data = open('nomes.txt','r')
-
 n_data = data.readlines()
 n_data = map(lambda s: s.strip(),n_data)
-
 
 if os.access(n_data[1],os.F_OK)== False:
     os.mkdir(n_data[1])
 os.chdir(n_data[1])
 
 tam=len(n_data)
-
 for num in range(5,tam-1):
     cert = '''
     \documentclass[12pt,a4paper]{memoir}
@@ -42,19 +36,11 @@ for num in range(5,tam-1):
         \HUGE{CERTIFICADO}\\\[50pt]
     \end{center}
     '''
-
-
-
-
-
     cert = cert+'''
     {\\fontsize{19pt}{24pt} \selectfont Certificamos que ''' + n_data[num]
-
     cert = cert+''' participou do '''+n_data[0]+''': "'''+n_data[1]+'''" no dia '''+n_data[2]+''', realizado pelo Programa de Educação Tutorial em Física (PET-FÍSICA) da Universidade Estadual de Maringá, com carga horária total de '''+n_data[3]+''' '''+n_data[4]+''' HORAS. }\\\[30pt]'''
-
     today = datetime.date.today()
     dia = today.strftime('%d de %B de %Y').lower()
-
     cert = cert+'''
     \\begin{center}
     {\\fontsize{19pt}{24pt} \selectfont Maringá-PR, '''+dia+'''.}\\\\[220pt]
